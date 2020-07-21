e7f00ca227bb70e47e90bff1d8b5b3966051348a
antoinefortin <fortin.antoine@cdrin.com>
2020-07-21 00:08:09 -0400

    First try

---
+++ DirtyOne/Conspiration/Assets/Script/Gameplay/PlayerTurnHandler.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTurnHandler : MonoBehaviour
+{
+
+    public GameObject[] Players;
+    private Player currentPlayer; // Reference to the script attached to "myEnemy" object.
+    public GameObject refBoardVerbose;
+
+    void Start()
+    {
+
+        // Force all player to to Hidle at start
+        for (int i = 0; i < Players.Length; i++)
+        {
+            Players[0].GetComponent<Player>().state = -1;
+        }
+
+        // Init First Player
+        currentPlayer = Players[0].GetComponent<Player>();
+        currentPlayer.state = 1;
+        ChangeDisplayBoardVerbose("Au tour de : " + currentPlayer.nameD +  " !!");
+
+        UpdateCurrentPlayerInfos(currentPlayer);
+
+
+    }
+
+    void UpdateCurrentPlayerInfos(
+        Player thePlayer
+    )
+    {
+        
+        GameObject refBoard = thePlayer.refCard;
+        // Get frame and change its color
+        foreach (Transform child in refBoard.transform)
+        {
+            if (child.name == "Frame")
+            {
+                GameObject frame = child.gameObject;
+                Renderer pionRenderer = frame.GetComponent<Renderer>();
+                pionRenderer.material.SetColor("_Color", Color.green);
+            }
+        }
+
+        thePlayer.state = 1;
+
+
+    }
+
+    void ChangeDisplayBoardVerbose(
+        string verboseText
+    )
+    {
+        TextMesh refText = refBoardVerbose.GetComponent<TextMesh>();
+        refText.text = verboseText;
+
+    }
+}
---
+++ DirtyOne/Conspiration/Assets/Script/PlayerManager/Player.cs
@@ -0,0 +1,228 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct PlayerState
+{
+    int isPlaying;
+    int isDonePlaying;
+    int isRollingDice;
+    int iHidling;
+}
+
+public class Player : MonoBehaviour
+{
+
+    // Player infos
+    public string nameD;
+    public string medias;
+    public Texture2D textureBuffer; // Not really
+    public int chloroquineAmount;
+    public int youtubeFollowers;
+    public int cash;
+
+    // References
+    public GameObject refCard;
+    public GameObject refPion;
+    public Color pionColor;
+
+    // State
+    public PlayerState playerState = new PlayerState();
+    public int state = 4;
+    public bool isPlaying = false;
+    public bool isWaitingForInput = true;
+
+    // Turn Infos
+    private int turnCounter = 0;
+    private int diceResult  = 0;
+    private bool hasRolledDice = false;
+
+
+    // Debug
+    private bool oncer = false;
+    private GameObject camGO;
+    private Camera theMainCam;
+    
+
+    private void InitPlayer()
+    {
+        // Walk each child object
+        foreach (Transform c in refCard.transform)
+        {
+            Debug.Log(c.name);
+            switch (c.name)
+            {
+                case "Picture":
+
+                    print("On Picture");
+                    MeshRenderer meshRenderer = c.GetComponent<MeshRenderer>();
+                    meshRenderer.material.SetTexture("_MainTex", textureBuffer);
+                                        
+                    break;
+
+                case "Infos":
+                    foreach (Transform sc in c.transform)
+                    {
+
+                        if (sc.name == "Static")
+                        {
+                            foreach (Transform ssc in sc.transform)
+                            {
+                                Debug.Log(ssc.name);
+                                if (ssc.name == "Name")
+                                {
+                                    TextMesh cur = ssc.GetComponent<TextMesh>();
+                                    cur.text = nameD;
+                                }
+
+                                if (ssc.name == "Media")
+                                {
+                                    TextMesh cur = ssc.GetComponent<TextMesh>();
+                                    cur.text = medias;
+                                }
+
+                                if (ssc.name == "ChloroquineAmount")
+                                {
+                                    TextMesh cur = ssc.GetComponent<TextMesh>();
+                                    cur.text = chloroquineAmount.ToString();
+                                }
+
+                                if (ssc.name == "YoutubeAmount")
+                                {
+                                    TextMesh cur = ssc.GetComponent<TextMesh>();
+                                    cur.text = youtubeFollowers.ToString();
+                                }
+
+                                if (ssc.name == "MoneyAmount")
+                                {
+                                    TextMesh cur = ssc.GetComponent<TextMesh>();
+                                    cur.text = cash.ToString();
+                                }
+
+
+                            }    
+                        }
+                    }
+                    break;
+                default:
+    
+                    break;
+            }
+        }
+
+    }
+
+    private void InitPion()
+    {
+       Renderer pionRenderer = refPion.GetComponent<Renderer>();
+       pionRenderer.material.SetColor("_Color", pionColor);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        camGO = GameObject.Find("Main Camera");
+
+        theMainCam = camGO.GetComponent<Camera>();
+
+        if (refCard)
+        {
+            InitPlayer();
+        }
+
+        InitPion();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleState();
+    }
+
+    void HandleState()
+    {
+        switch (state)
+        {
+            case 4:
+                // Add more state later...
+                break;
+            case 3:
+                // Add more state later...
+                break;
+            case 2:
+
+                break;
+            case 1:
+                // Waiting for Input as init.
+                isWaitingForInput = true;
+                DoPlayerTurn();
+                
+                break;
+            case 0:
+                DoPlayerIsWaitingToPlay();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void DoPlayerIsWaitingToPlay()
+    {
+        
+    }
+
+
+    void DoPlayerTurn()
+    {
+
+        Debug.Log(nameD + "Is playing");
+        if (isWaitingForInput)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = theMainCam.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+
+                    // Hit button dice
+                    if (hit.collider.gameObject.name == "RollDiceButton")
+                    {
+                        diceResult = GetDiceNumber();
+                        hasRolledDice = true;
+                    }
+                    
+                }
+            }
+
+            if (hasRolledDice)
+            {
+
+                MovePionOnBoard(diceResult);
+            }
+        }
+    }
+
+
+    void MovePionOnBoard(
+        int move
+    )
+    {
+        refPion.transform.position = 
+            new Vector3(refPion.transform.position.x,
+                        refPion.transform.position.y,
+                         refPion.transform.position.y + move
+                        );
+        Debug.Log(move);
+    }
+
+    int GetDiceNumber()
+    {
+        Debug.Log(diceResult);
+        return Random.Range(1,7);
+    }
+}
+
+
